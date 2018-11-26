f61601c2ebc4775c15fc337416de934e1ef0ec98
Dan Binder <djbinder@gmail.com>
2018-11-26 16:17:36 -0600

    updates to group game won outcome

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 using System;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-
+using Microsoft.Extensions.Logging;
 
 namespace movieGame {
 
@@ -10,12 +10,26 @@ namespace movieGame {
         public static void Main (string[] args) {
             Console.WriteLine ($"Version: {Environment.Version}");
 
+            if(Environment.Version.ToString() == "Production")
+            {
+                Console.WriteLine();
+                Console.WriteLine("***************************************************");
+                Console.WriteLine("SWITCH TO DEV ENVIRONMENT");
+                Console.WriteLine("***************************************************");
+                Console.WriteLine();
+            }
+
+
             CreateWebHostBuilder (args).Build ().Run ();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder (string[] args) =>
             WebHost.CreateDefaultBuilder (args)
-            .UseStartup<Startup> ();
+            .UseSetting(WebHostDefaults.SuppressStatusMessagesKey, "False")
+            .UseStartup<Startup> ()
+            .ConfigureLogging(logging =>
+                    logging.SetMinimumLevel(LogLevel.Warning)
+                );
     }
 }
 
